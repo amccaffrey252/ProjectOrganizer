1bc83f0f0c13f1fe7c6541827f848eb001237888
shlocko <mcdude22@gmail.com>
2022-02-08 11:02:16 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+    const [columns, setColumns] = useState([]);
+    
+    
+    return (
+    <div className="App">
+    
+    </div>
+    )
+}
+
+export default App
---
+++ src/classes/Column.ts
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+class Column{
+    name: string;
+    cards: Card[] = [];
+    
+    constructor(name: string) {
+        this.name = name;
+    }
+    
+    addCard(){
+    
+    }
+    removeCard(){
+    
+    }
+    
+}
